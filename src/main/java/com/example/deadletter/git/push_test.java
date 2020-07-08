44d67ea0b5bcdc2087f01361c0bd4915b27a4282
wulh <longhui.wu@fujfu.com>
2020-07-08 10:57:58 +0800

    commit 3

--- src/main/java/com/example/deadletter/git/push_test.java
+++ src/main/java/com/example/deadletter/git/push_test.java
@@ -9,5 +9,6 @@ public class push_test {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
